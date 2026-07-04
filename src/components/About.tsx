1ca4092a3287fefacc9fb4b147b24c42d65b0ec8
Nitish Bainsla <127420743+nitishbainslaa@users.noreply.github.com>
2026-07-04 15:43:23 +0530

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -101,7 +101,7 @@ export default function About() {
             <div className="relative h-162.5 overflow-hidden rounded-[40px]">
 
             <Image
-              src="/images/map.jpg"
+              src="/images/about.jpg"
               alt="About LP Law India"
               fill
               sizes="(max-width: 768px) 100vw, 50vw"
