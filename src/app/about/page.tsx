9e882d670056f940f0d1696f3426dbceb9be8228
Nitish Bainsla <127420743+nitishbainslaa@users.noreply.github.com>
2026-07-04 15:54:36 +0530

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,13 +1,9 @@
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-import Partners from "@/components/Partners";
-
 import AboutIntro from "@/components/about/AboutIntro";
 import WhoWeAre from "@/components/about/WhoWeAre";
 import CoreValues from "@/components/about/CoreValues";
 import WhyChooseUs from "@/components/about/WhyChooseUs";
-//import Gallery from "@/components/about/Gallery";
-//import CTA from "@/components/about/CTA";
 
 export default function AboutPage() {
   return (
@@ -18,7 +14,6 @@ export default function AboutPage() {
 
         <AboutIntro />
         <WhoWeAre />
-        <Partners />
         <CoreValues />
         <WhyChooseUs />
       </main>
